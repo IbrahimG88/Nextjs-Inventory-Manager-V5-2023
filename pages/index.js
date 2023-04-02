32711a2d1a424f65cb20fc3f9a5c8367e8602712
IbrahimG88 <goharyibrahim@gmail.com>
2023-04-02 16:24:26 +0200

    working on home screen navigation

--- pages/index.js
+++ pages/index.js
@@ -1,109 +1,81 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { connectToDatabase } from "../lib/db";
-import { getPreviousDate } from "../lib/helpers/get-set-dates";
-import { getNowDate } from "../lib/helpers/get-set-dates";
-import { getConsumptionData } from "../lib/helpers/get-consumption-data";
-import { subtractArrays } from "@/lib/helpers/subtract-Consumption";
+import * as React from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+import Box from "@mui/material/Box";
+import Link from "next/link";
+import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutlined";
+import CreateNewFolderOutlinedIcon from "@mui/icons-material/CreateNewFolderOutlined";
+import Inventory2OutlinedIcon from "@mui/icons-material/Inventory2Outlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 
-// Use serverSideProps to fetch data on each request
-export async function getServerSideProps() {
-  // Connect to MongoDB Atlas
+import { styled } from "@mui/material/styles";
 
-  // Use try/catch blocks to handle errors
-  try {
-    const client = await connectToDatabase();
-    const db = client.db("myFirstDatabase");
-    const collectionAppVariables = db.collection("appVariables");
-    const collectionInventory2 = db.collection("inventory2");
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2";
 
-    // for more processing add another const collectionInventory2 = db.collection("inventory2");
-
-    // Find one document in appVariables collection where date2 exists
-    const appVariable = await collectionAppVariables.findOne({
-      date2: { $exists: true },
-    });
-    if (!appVariable) {
-      return res.status(404).json({ message: "date2 not found" });
-    }
-
-    // Use JSON.stringify() to convert date2 into a string
-    const dateValue = JSON.stringify(appVariable.date2);
-
-    const testsListFromMongo = await collectionInventory2.findOne({});
-    if (!testsListFromMongo) {
-      return res.status(404).json({ message: "testsList not loaded " });
-    }
-
-    // get the consumptionArray
-    const previousDateDetails = getPreviousDate(dateValue);
-    const nowDateDetails = getNowDate();
-    const consumptionArray = await getConsumptionData(
-      previousDateDetails,
-      nowDateDetails
-    );
-
-    // update the date only if you received the consumption data array
-    if (consumptionArray.length >= 1) {
-      // set Mongo Date to now add a conditional later to only update the date if the data in mongo was updated  also here in getServerSideProps
-      // if no date2 was found set the date2 value to now
-      const nowDate = await collectionAppVariables.updateOne(
-        { date2: { $exists: true } },
-        { $currentDate: { date2: true } },
-        { upsert: true }
-      );
-      if (!nowDate) {
-        return res
-          .status(404)
-          .json({ message: "now Date on Mongo not found " });
-      }
-    }
-
-    // get the testsList from MongoDB which is Array B:
-
-    // calculate the consumption from the testsList in MongoDB
-    // need to add the testsList
-    const applyConsumptionToMongoData = subtractArrays(
-      JSON.parse(consumptionArray),
-      testsListFromMongo
-    );
-
-    // update the testsList in MongoDB with the updated consumption
-    const query = { testsList: { $exists: true } };
-    const updateDocument = { $set: { testsList: applyConsumptionToMongoData } };
-    const result = await collectionInventory2.updateOne(query, updateDocument);
-
-    return {
-      props: {
-        dateValue: dateValue,
-        consumptionArray: consumptionArray,
-        applyConsumptionToMongoData: applyConsumptionToMongoData,
-      },
-    };
-  } catch (error) {
-    // Log the error message and return an empty props object
-    console.error(error.message);
-  }
-}
-
-export default function Home(props) {
-  const dateValue = props.dateValue;
-  console.log("getPreviousDate()", getPreviousDate(dateValue));
-  const previousDateDetails = getPreviousDate(dateValue);
-  console.log("getNowDate()", getNowDate());
-  const nowDateDetails = getNowDate();
-  console.log("now hours", nowDateDetails.hours);
-  const consumptionArray = props.consumptionArray;
-  console.log("consumptionArray", consumptionArray);
-  const applyConsumptionToMongoData = props.applyConsumptionToMongoData;
-  console.log("applyConsumptionToMongoData", applyConsumptionToMongoData);
+export default function Home() {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.primary,
+    fontWeight: "bold",
+    ":hover": {
+      color: "green",
+    },
+  }));
 
   return (
-    <>
-      <h1>Value for get Date: {dateValue}</h1>
-    </>
+    <div className="p-8">
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container justifyContent="center" spacing={2}>
+          <Grid item xs={4}>
+            <Grid container direction="column" spacing={2}>
+              <Grid item>
+                <Link href="/mui-table-search">
+                  <Item>
+                    <Inventory2OutlinedIcon />
+                    Inventory
+                  </Item>
+                </Link>
+              </Grid>
+              <Grid item>
+                <Item>
+                  <Link href="/expiree-notifications">
+                    <NotificationsOutlinedIcon />
+                    Notifications
+                  </Link>
+                </Item>
+              </Grid>
+              <Grid item>
+                <Item>
+                  <Link href="/stocks-to-add-3">
+                    <AddShoppingCartOutlinedIcon />
+                    Add Stocks
+                  </Link>
+                </Item>
+              </Grid>
+              <Grid item>
+                <Item>
+                  <Link href="/create-order">
+                    <CreateNewFolderOutlinedIcon />
+                    Create Order
+                  </Link>
+                </Item>
+              </Grid>
+              <Grid item>
+                <Item>
+                  <Link href="/settings">
+                    <SettingsOutlinedIcon />
+                    Settings
+                  </Link>
+                </Item>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
   );
 }
