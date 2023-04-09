92e9bf90ccb1ea9ff16186a530f82d6bf40adc8e
IbrahimG88 <goharyibrahim@gmail.com>
2023-04-09 15:57:05 +0200

    updated api cron job

--- pages/api/vercel-cron.js
+++ pages/api/vercel-cron.js
@@ -1,13 +1,9 @@
-export async function startCronJob() {
+export async function handler(req, res) {
   // Schedule tasks to be run on the server.
 
   console.log("running updateAppData every minute");
 
   // Call the API route to run the update app data code
   await fetch(`${process.env.APP_URL}/update-app-data-node-cron`);
+  res.status(200).json("Data updated successfully");
 }
-
-// run the fuunction:
-startCronJob();
-
-// not using this file now
