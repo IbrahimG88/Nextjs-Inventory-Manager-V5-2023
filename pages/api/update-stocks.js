8316ba963ab3fa8a0b2931545b20272b5a20fb37
IbrahimG88 <goharyibrahim@gmail.com>
2023-05-11 15:25:33 +0200

    removed excess code

--- pages/api/update-stocks.js
+++ pages/api/update-stocks.js
@@ -24,8 +24,6 @@ export default async function handler(req, res) {
     };
     const result = await collection.updateOne(filter, update);
 
-    client.close();
-
     res.status(200).json({ message: "Item updated successfully" });
   } catch (error) {
     res.status(500).json({ message: "Error updating item" });
